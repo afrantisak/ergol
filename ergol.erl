698c72568aa895632f3236c302ffe83f0363fc47
Aaron Frantisak <afrantisak@gmail.com>
2014-06-04 17:20:31 -0500

    close

--- ergol.erl
+++ ergol.erl
@@ -1,7 +1,14 @@
 -module(ergol).
 
 -export([main/0]).
--export([evolve/1, get/3, set/4, replace/3]).
+-export([neighbors/3, neighbors/7, evolve/1, evolve/6, get/3, set/4, replace/3, gen/0]).
+
+gen() ->
+    [[0, 1, 0, 0, 0],
+     [1, 0, 0, 1, 1],
+     [1, 1, 0, 0, 1],
+     [0, 1, 0, 0, 0],
+     [1, 0, 0, 0, 1]].
 
 main() ->
     Gen1 = [[0, 1, 0, 0, 0],
@@ -18,15 +25,27 @@ main() ->
     io:format("Passed~n").
     
 evolve(Gen1) ->
-    evolve(Gen1, Gen1, 1, 1).
+    evolve(Gen1, Gen1, 1, 1, 5, 5).
 
-evolve(Gen1, Gen2, X, Y) ->
-    case get(Gen1, X, Y) of
-        1 -> live(Gen1, Gen2, X, Y);
-        0 -> dead(Gen1, Gen2, X, Y)
+evolve(Gen1, Gen2, X, Y, MaxX, MaxY) ->
+    Gen3 = case get(Gen1, X, Y) of
+               1 -> live(Gen1, Gen2, X, Y);
+               0 -> dead(Gen1, Gen2, X, Y)
+           end,
+    case Y of
+        MaxY -> 
+            case X of
+                MaxX ->
+                    Gen3;
+                _ ->
+                    evolve(Gen1, Gen3, X + 1, 1, MaxX, MaxY)
+            end;
+        _ ->
+            evolve(Gen1, Gen3, X, Y + 1, MaxX, MaxY)
     end.
 
 dead(Gen1, Gen2, X, Y) ->
+    io:format("dead~n"),
     case neighbors(Gen1, X, Y) of
         2 -> set(Gen2, X, Y, 1);
         3 -> set(Gen2, X, Y, 1);
@@ -34,13 +53,35 @@ dead(Gen1, Gen2, X, Y) ->
     end.
 
 live(Gen1, Gen2, X, Y) ->
+    io:format("live~n"),
     case neighbors(Gen1, X, Y) of
         3 -> set(Gen2, X, Y, 1);
         _ -> set(Gen2, X, Y, 0)
     end.
 
-neighbors(_Gen, _X, _Y) -> 
-    0. % TODO
+neighbors(Gen1, X, Y) ->
+    L = erlang:max(1, X - 1),
+    R = erlang:min(5, X + 1),
+    T = erlang:max(1, Y - 1),
+    B = erlang:min(5, Y + 1),
+    Sum = neighbors(Gen1, 0, L, T, R, B, T),
+    SumMinus = Sum - get(Gen1, X, Y),
+    io:format("X: ~p (~p, ~p) Y: ~p (~p, ~p) Sum: ~p~n", [X, L, R, Y, T, B, SumMinus]),
+    SumMinus.
+
+neighbors(Gen1, Total, X, Y, MaxX, MaxY, MinY) ->
+    Total1 = Total + get(Gen1, X, Y),
+    case Y of
+        MaxY -> 
+            case X of
+                MaxX ->
+                    Total1;
+                _ ->
+                    neighbors(Gen1, Total1, X + 1, MinY, MaxX, MaxY, MinY)
+            end;
+        _ ->
+            neighbors(Gen1, Total1, X, Y + 1, MaxX, MaxY, MinY)
+    end.
 
 get(Gen, X, Y) ->
     lists:nth(X, lists:nth(Y, Gen)).
