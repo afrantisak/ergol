98289cc75e87f2d2dd798dac8e18db2c98941c63
Aaron Frantisak <afrantisak@gmail.com>
2014-06-04 21:56:03 -0500

    griderate it

--- ergol.erl
+++ ergol.erl
@@ -1,6 +1,6 @@
 -module(ergol).
 
--export([main/0]).
+-export([test/0]).
 
 % "Game of Life".
 % The input will be a game board of cells, either alive (1) or dead (0).
@@ -12,7 +12,7 @@
 %  3) Any live cell with more than three live neighbours dies (overcrowding)
 %  4) Any dead cell with exactly three live neighbours becomes a live cell (reproduction)
 
-main() ->
+test() ->
     Gen1 = [[0, 1, 0, 0, 0],
             [1, 0, 0, 1, 1],
             [1, 1, 0, 0, 1],
@@ -27,24 +27,38 @@ main() ->
     io:format("Passed~n").
     
 evolve(Gen1) ->
-    evolve(Gen1, Gen1, 1, 1, 5, 5).
+    griderate(Gen1, Gen1, fun evolve1/4, 1, 1, 5, 5, 1).
 
-evolve(Gen1, Gen2, X, Y, MaxX, MaxY) ->
-    Gen3 = case get(Gen1, X, Y) of
-               1 -> live(Gen1, Gen2, X, Y);
-               0 -> dead(Gen1, Gen2, X, Y)
-           end,
+evolve1(Gen1, Gen2, X, Y) ->
+    case get(Gen1, X, Y) of
+        1 -> live(Gen1, Gen2, X, Y);
+        0 -> dead(Gen1, Gen2, X, Y)
+    end.
+
+griderate(Input, Output, Function, X, Y, MaxX, MaxY, MinY) ->
+    %% griderate == grid + iterate.  get it?
+    Output1 = Function(Input, Output, X, Y),
     case Y of
-        MaxY -> 
+        MaxY ->
             case X of
                 MaxX ->
-                    Gen3;
+                    Output1;
                 _ ->
-                    evolve(Gen1, Gen3, X + 1, 1, MaxX, MaxY)
+                    griderate(Input, Output1, Function, X + 1, MinY, MaxX, MaxY, MinY)
             end;
         _ ->
-            evolve(Gen1, Gen3, X, Y + 1, MaxX, MaxY)
+            griderate(Input, Output1, Function, X, Y + 1, MaxX, MaxY, MinY)
     end.
+
+get_sum(Gen1, Total, X, Y) ->
+    Total + get(Gen1, X, Y).
+
+neighbors(Gen1, X, Y) ->
+    L = erlang:max(1, X - 1),
+    R = erlang:min(5, X + 1),
+    T = erlang:max(1, Y - 1),
+    B = erlang:min(5, Y + 1),
+    griderate(Gen1, 0, fun get_sum/4, L, T, R, B, T) - get(Gen1, X, Y).
 
 live(Gen1, Gen2, X, Y) ->
     case neighbors(Gen1, X, Y) of
@@ -57,27 +71,6 @@ dead(Gen1, Gen2, X, Y) ->
     case neighbors(Gen1, X, Y) of
         3 -> set(Gen2, X, Y, 1);
         _ -> set(Gen2, X, Y, 0)
-    end.
-
-neighbors(Gen1, X, Y) ->
-    L = erlang:max(1, X - 1),
-    R = erlang:min(5, X + 1),
-    T = erlang:max(1, Y - 1),
-    B = erlang:min(5, Y + 1),
-    Sum = neighbors(Gen1, 0, L, T, R, B, T) - get(Gen1, X, Y).
-
-neighbors(Gen1, Total, X, Y, MaxX, MaxY, MinY) ->
-    Total1 = Total + get(Gen1, X, Y),
-    case Y of
-        MaxY -> 
-            case X of
-                MaxX ->
-                    Total1;
-                _ ->
-                    neighbors(Gen1, Total1, X + 1, MinY, MaxX, MaxY, MinY)
-            end;
-        _ ->
-            neighbors(Gen1, Total1, X, Y + 1, MaxX, MaxY, MinY)
     end.
 
 get(Gen, X, Y) ->
---
+++ readme.md
@@ -0,0 +1,12 @@
+## Erlang implementation of "Game of Life"
+
+## To run:
+
+    $ erl
+
+inside erlang repl:
+
+    c(ergol).
+    ergol:test().
+
+If it passed (Gen1 evolves into Gen2 as expected) it will report passed.  If it failed it will report the result.
