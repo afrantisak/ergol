762200d85f87f87cc06c9c600f6a182b85f1882f
Aaron Frantisak <afrantisak@gmail.com>
2014-10-06 17:13:29 -0500

    use tuples to save some typing

--- golf.erl
+++ golf.erl
@@ -1,16 +1,14 @@
-
+#!/usr/bin/env escript
 -mode(compile).
 main(_)->[[0,0,0,0,0],[1,0,1,1,1],[1,1,1,1,1],[0,1,0,0,0],[0,0,0,0,0]]=e([[0,1,0,0,0],[1,0,0,1,1],[1,1,0,0,1],[0,1,0,0,0],[1,0,0,0,1]]).
 e(1,2)->1;
-e(1,3)->1;
-e(1,_)->0;
-e(0,3)->1;
-e(0,_)->0.
-e(I)->i(I,I,fun e/8,1,1,1,1,l(n(1,I)),l(I)).
-e(I,O,X,Y,Xa,Ya,Xz,Yz)->sv(O,X,Y,e(gv(I,X,Y),nb(I,X,Y,Xa,Ya,Xz,Yz))).
-i(Ib,Ob,F,X,Y,Xa,Ya,Xz,Yz)->Ob1=F(Ib,Ob,X,Y,Xa,Ya,Xz,Yz),case Y of Yz->case X of Xz->Ob1;_->i(Ib,Ob1,F,X+1,Ya,Xa,Ya,Xz,Yz)end;_->i(Ib,Ob1,F,X,Y+1,Xa,Ya,Xz,Yz)end.
-nb(Ib,X,Y,Xa,Ya,Xz,Yz)->X1=mx(Xa,X-1),Y1=mx(Ya,Y-1),i(Ib,0,fun sum/8,X1,Y1,X1,Y1,mn(Xz,X+1),mn(Yz,Y+1))-gv(Ib,X,Y).
-sum(Ib,T,X,Y,_,_,_,_)->T+gv(Ib,X,Y).
+e(_,3)->1;
+e(_,_)->0.
+e(I)->i(I,I,fun e/5,1,1,{1,1,l(n(1,I)),l(I)}).
+e(I,O,X,Y,S)->sv(O,X,Y,e(gv(I,X,Y),nb(I,X,Y,S))).
+i(Ib,Ob,F,X,Y,S={_,Ya,Xz,Yz})->Ob1=F(Ib,Ob,X,Y,S),case Y of Yz->case X of Xz->Ob1;_->i(Ib,Ob1,F,X+1,Ya,S)end;_->i(Ib,Ob1,F,X,Y+1,S)end.
+nb(Ib,X,Y,{Xa,Ya,Xz,Yz})->X1=mx(Xa,X-1),Y1=mx(Ya,Y-1),i(Ib,0,fun sum/5,X1,Y1,{X1,Y1,mn(Xz,X+1),mn(Yz,Y+1)})-gv(Ib,X,Y).
+sum(Ib,T,X,Y,_)->T+gv(Ib,X,Y).
 gv(I,X,Y)->n(X,n(Y,I)).
 sv(I,X,Y,V)->lr(Y,lr(X,V,n(Y,I)),I).
 lr(I,V,A)->{L,[_|R]}=ls(I-1,A),la([L,[V],R]).
