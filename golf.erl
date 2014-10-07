dc492a3f8ee4885e61b2148f859f1dd65887781f
Aaron Frantisak <afrantisak@gmail.com>
2014-10-07 01:15:29 -0500

    golfing

--- golf.erl
+++ golf.erl
@@ -1,10 +1,14 @@
 #!/usr/bin/env escript
 -mode(compile).
-main(_)->[[0,0,0,0,0],[1,0,1,1,1],[1,1,1,1,1],[0,1,0,0,0],[0,0,0,0,0]]=e([[0,1,0,0,0],[1,0,0,1,1],[1,1,0,0,1],[0,1,0,0,0],[1,0,0,0,1]]).
+main(_)->
+    O=[[0,0,0,0,0],[1,0,1,1,1],[1,1,1,1,1],[0,1,0,0,0],[0,0,0,0,0]],
+    I=[[0,1,0,0,0],[1,0,0,1,1],[1,1,0,0,1],[0,1,0,0,0],[1,0,0,0,1]],
+    O=e(I),
+    [io:format("~p~n",[R])||R<-O].
 e(1,2)->1;e(_,3)->1;e(_,_)->0.
 e(I)->i(I,I,fun e/4,{1,1},{1,1,l(n(1,I)),l(I)}).
 e(I,O,C,S)->sv(O,C,e(gv(I,C),nb(I,C,S))).
-i(I,O,F,C={X,Y},S={_,Ya,Xz,Yz})->O1=F(I,O,C,S),case Y of Yz->case X of Xz->O1;_->i(I,O1,F,{X+1,Ya},S)end;_->i(I,O1,F,{X,Y+1},S)end.
+i(I,O,F,C={X,Y},S={Xa,_,Xz,Yz})->O1=F(I,O,C,S),case X of Xz->case Y of Yz->O1;_->i(I,O1,F,{Xa,Y+1},S)end;_->i(I,O1,F,{X+1,Y},S)end.
 nb(I,C={X,Y},{Xa,Ya,Xz,Yz})->X1=mx(Xa,X-1),Y1=mx(Ya,Y-1),i(I,0,fun sum/4,{X1,Y1},{X1,Y1,mn(Xz,X+1),mn(Yz,Y+1)})-gv(I,C).
 sum(I,T,C,_)->T+gv(I,C).
 gv(I,{X,Y})->n(X,n(Y,I)).
