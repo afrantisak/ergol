8ea26ce15710e9d4ff1ec0e9b9ff0506a92dcfdd
Aaron Frantisak <afrantisak@gmail.com>
2014-10-07 02:16:47 -0500

    remove last of 2-char names

--- golf.erl
+++ golf.erl
@@ -1,13 +1,13 @@
 #!/usr/bin/env escript
 main(_)->[[0,0,0,0,0],[1,0,1,1,1],[1,1,1,1,1],[0,1,0,0,0],[0,0,0,0,0]]=e([[0,1,0,0,0],[1,0,0,1,1],[1,1,0,0,1],[0,1,0,0,0],[1,0,0,0,1]]).
 e(1,2)->1;e(_,3)->1;e(_,_)->0.
-e(I)->i(I,fun(J,C,S)->e(v(J,C),n(J,C,S))end,{1,1,l(n(1,I)),l(I)}).
-i(I,F,S={Xa,Ya,Xz,Yz})->[[F(I,{X,Y},S)||X<-r(Xa,Xz)]||Y<-r(Ya,Yz)].
-n(I,C={X,Y},{Xa,Ya,Xz,Yz})->a([a(R)||R<-i(I,fun(J,D,_)->v(J,D)end,{x(Xa,X-1),x(Ya,Y-1),m(Xz,X+1),m(Yz,Y+1)})])-v(I,C).
+e(I)->i(I,fun(J,P,S)->e(v(J,P),n(J,P,S))end,{1,1,l(n(1,I)),l(I)}).
+i(I,F,S={A,B,C,D})->[[F(I,{X,Y},S)||X<-r(A,C)]||Y<-r(B,D)].
+n(I,P={X,Y},{A,B,C,D})->a([a(R)||R<-i(I,fun(J,E,_)->v(J,E)end,{x(A,X-1),x(B,Y-1),m(C,X+1),m(D,Y+1)})])-v(I,P).
 v(I,{X,Y})->n(X,n(Y,I)).
-n(I,A)->lists:nth(I,A).
+n(I,L)->lists:nth(I,L).
 r(L,R)->lists:seq(L,R).
 x(L,R)->erlang:max(L,R).
 m(L,R)->erlang:min(L,R).
-l(A)->length(A).
-a(A)->lists:sum(A).
+l(L)->length(L).
+a(L)->lists:sum(L).
